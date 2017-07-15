ca63c1650bf3dee39769ff090119a100f63518f8
Rostyslav Gutiy <burbigo123@gmail.com>
2017-07-15 14:16:27 +0300

    changed name
    index.tsx --> app.tsx

---
+++ src/app.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+import { Todo } from './components/Todo';
+import './assets/styles/main.scss';
+
+ReactDOM.render(<Todo />, document.querySelector('.app'));
