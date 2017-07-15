11f098cff226607a70fbeee0e5333de7a69b65b0
Rostyslav Gutiy <burbigo123@gmail.com>
2017-07-15 14:14:11 +0300

    added test .tsx and .scss files

---
+++ src/components/Todo.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+export class Todo extends React.Component<{}, {}> {
+    render() {
+        return (
+            <div>
+                <h1>React Todo App</h1>
+            </div>
+        );
+    };
+}
